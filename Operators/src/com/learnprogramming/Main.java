6ceacba8097afd583434843d2f0b7566a915aea0
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-08-05 16:47:42 +0700

    Abbreviating Operators

--- Operators/src/com/learnprogramming/Main.java
+++ Operators/src/com/learnprogramming/Main.java
@@ -23,6 +23,28 @@ public class Main {
         result = result % 3; // the remainder of (4 % 3) =  1
         System.out.println("4 % 3 = " + result);
 
+        // result = result + 1;
+        result++; // 1 + 1 = 2
+        System.out.println("1 + 1 = " + result);
+
+        result--; // 2 - 1 = 1
+        System.out.println("2 - 1 = " + result);
+
+        // result = result + 2;
+        result += 2; // 1 + 2 = 3
+        System.out.println("1 + 2 = " + result);
+
+        // result = result * 10;
+        result *= 10; // 3 * 10 = 30
+        System.out.println("3 * 10 = " + result);
+
+        // result = result / 3
+        result /= 3; // 30 / 3 = 10
+        System.out.println("30 / 3 = " + result);
+
+        // result = result - 2
+        result -= 2; // 10 - 2 = 8
+        System.out.println("10 - 2 = " + result);
 
     }
 }
