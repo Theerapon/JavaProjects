c9ac07f2b165e95d1597e96253cc71d92ce3ab96
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-08-05 20:50:04 +0700

    Method 2

--- Methods/src/com/learnprogramming/Main.java
+++ Methods/src/com/learnprogramming/Main.java
@@ -31,14 +31,27 @@ public class Main {
     }
 
     public  static  int calculateHighScorePosition(int playerScore) {
+
+        int position = 4;
+
         if (playerScore >= 1000) {
-            return  1;
-        } else if (playerScore >= 500 && playerScore < 1000) {
-            return 2;
-        } else if (playerScore >= 100 && playerScore < 500) {
-            return 3;
-        } else {
-            return  4;
+            position = 1;
+        } else if (playerScore >= 500) {
+            position = 2;
+        } else if (playerScore >= 100) {
+            position = 3;
         }
+
+        return  position;
+
+        /*if (playerScore >= 1000) {
+            return  1;
+        } else if (playerScore >= 500) {
+            return 2;
+        } else if (playerScore >= 100) {
+            return 3;
+        }
+        return  4;*/
+
     }
 }
