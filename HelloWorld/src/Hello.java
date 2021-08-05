f76e89d88a7df233809bddb8e996837e732e3e09
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-08-05 14:05:49 +0700

    Variables

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,6 +1,11 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class Hello {
 
     public  static  void main(String[] args) {
          System.out.println("Hello World");
+
+         int myFirstNumber = 5 + 10;
+         System.out.println("MyFirstNumber = " + myFirstNumber);
     }
 }
