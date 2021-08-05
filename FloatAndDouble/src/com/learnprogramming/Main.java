4cb3c52d5081ebc8f4c1adbe8b202753fc8b3906
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-08-05 15:33:17 +0700

    Float Precision

--- FloatAndDouble/src/com/learnprogramming/Main.java
+++ FloatAndDouble/src/com/learnprogramming/Main.java
@@ -14,8 +14,21 @@ public class Main {
         System.out.println("Double minimum value = " + myMinDoubleValue);
         System.out.println("Double maximum value = " + myMaxDoubleValue);
 
-        int myIntValue = 5;
-        float myFloatValue = 5.25f;
-        double myDoubleValue = 5.25d;
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5d / 3d;
+        System.out.println("MyINtValue = " + myIntValue);
+        System.out.println("MyFloatValue = " + myFloatValue);
+        System.out.println("MyDoubleValue = " + myDoubleValue);
+
+        double INST_POUND = 0.45359237;
+        int pounds = 1;
+        double result_of_kilograms = pounds * INST_POUND;
+        System.out.println(pounds + " Pounds = " + result_of_kilograms + " Kilograms");
+
+        double pi = 3.1415927d;
+        double anotherNumber = 3_000_000.4_567_890d;
+        System.out.println("pi = " + pi);
+        System.out.println("anotherNumber = " + anotherNumber);
     }
 }
