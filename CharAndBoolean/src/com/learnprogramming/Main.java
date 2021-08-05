d66a90835a8967c00ddb8337fa8d22aede075275
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-08-05 15:52:44 +0700

    Char and Boolean

---
+++ CharAndBoolean/src/com/learnprogramming/Main.java
@@ -0,0 +1,20 @@
+package com.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'd';
+        char myUnicodeChar = '\u0044';
+        System.out.println("myChar = " + myChar);
+        System.out.println("MyUnicode = " + myUnicodeChar);
+
+        char myCopyrightChar = '\u00A9';
+        System.out.println("MyUnicode Copyright = " + myCopyrightChar);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+    }
+}
