67c84a3906c76800d6fdee9e37c3c09d0839b627
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-08-05 14:12:29 +0700

    Variable int

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -5,7 +5,14 @@ public class Hello {
     public  static  void main(String[] args) {
          System.out.println("Hello World");
 
-         int myFirstNumber = 5 + 10;
-         System.out.println("MyFirstNumber = " + myFirstNumber);
+         int myFirstNumber = (10 + 5) + (2 * 10);
+         int mySecondNumber = 12;
+         int myThirdNumber = myFirstNumber * 2;
+         int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+         int myLastOne = 1000 - myTotal;
+
+         System.out.println("MyTotalNumber = " + myTotal);
+        System.out.println("MyLastOne = " + myLastOne);
     }
 }
