d43e1bcc5c91293b22ac09afae66aa3f696ebe85
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-08-05 14:51:31 +0700

    Byte Shot Int Long

--- ByteShortIntLong/src/com/learnprogramming/Main.java
+++ ByteShortIntLong/src/com/learnprogramming/Main.java
@@ -16,5 +16,26 @@ public class Main {
         System.out.println("Bused MIN value = " + (myMinIntValue - 1));
 
         int myMaxIntTest = 2_147_483_647;
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+        System.out.println("Byte Maximum Value = " + myMaxByteValue) ;
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Minimum Value = " + myMinShortValue);
+        System.out.println("Short Maximum Value = " + myMaxShortValue);
+
+        long myLongValue = 100L;
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        System.out.println("Long Maximum Value = " + myMaxLongValue);
+
+        long bigLongLiteralValue = 2_147_483_647_234L;
+        System.out.println(bigLongLiteralValue);
+        
+        short bigShortLiteralValue = 32767;
     }
 }
