9e391ccd9278cb9d3ea19a6ea124bcc5aaf6633b
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-08-06 14:33:54 +0700

    Overloading

---
+++ Overloading/src/com/learnprogramming/Main.java
@@ -0,0 +1,32 @@
+package com.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int newScore = calculateScore("Tim", 500);
+        System.out.println("New score is " + newScore);
+        calculateScore(75);
+        calculateScore();
+    }
+
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("Player " + playerName + " scored " + score + " points.");
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score) {
+        System.out.println("Unnamed player scored " + score + " points.");
+        return score * 1000;
+    }
+
+    public static int calculateScore() {
+        System.out.println("No player name, no player score.");
+        return 0;
+    }
+
+    //this is error
+    /*public static void calculateScore() {
+        System.out.println("No player name, no player score.");
+    }*/
+
+}
