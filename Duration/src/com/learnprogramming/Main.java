b37fba1757539cd94c614216edbf81974c204f29
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-08-06 15:34:05 +0700

    Update Second and Minute to hh/mm/ss

--- Duration/src/com/learnprogramming/Main.java
+++ Duration/src/com/learnprogramming/Main.java
@@ -4,27 +4,24 @@ import com.sun.nio.sctp.AbstractNotificationHandler;
 
 public class Main {
 
+    private static final String INST_INVALID_VALUE = "Invalid value";
+    private static final int INST_SECOND_PER_MINUTE = 60;
+
     public static void main(String[] args) {
         System.out.println(getDurationString(3657));
         System.out.println(getDurationString(54, 23));
         System.out.println(getDurationString(61, 0));
     }
     public static String getDurationString(int minutes, int seconds) {
-
-        String str = "Invalid value";
-        if(minutes >= 0 && seconds >= 0) {
-            int tempMinutes = minutes + (seconds / 60);
-            int tempSeconds = seconds % 60;
-            int tempHour = tempMinutes / 60;
-            tempMinutes %= 60;
-            str = String.format("%02dh %02dm %02ds", tempHour, tempMinutes, tempSeconds);
-        }
-        return str;
+        return getDurationStringBySeconds((minutes * INST_SECOND_PER_MINUTE) + seconds);
     }
 
     public static String getDurationString(int seconds) {
+        return getDurationStringBySeconds(seconds);
+    }
 
-        String str = "Invalid value";
+    public static String getDurationStringBySeconds(int seconds) {
+        String str = INST_INVALID_VALUE;
         if(seconds >= 0) {
             int tempMinutes = seconds / 60;
             int tempSeconds = seconds % 60;
