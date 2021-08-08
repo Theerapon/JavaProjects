a46e20ca90aec26322b025cb1ab060107a0518ec
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-08-08 14:35:48 +0700

    Scanner Validate next int

--- ReadingUserInput/src/com/learnprogramming/Main.java
+++ ReadingUserInput/src/com/learnprogramming/Main.java
@@ -6,18 +6,27 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Enter your Age: ");
-        int age = scanner.nextInt();
-        scanner.nextLine(); // handle next line character (enter key)
+        boolean hasNextInt = scanner.hasNextInt();
+        if (hasNextInt) {
 
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
+            int age = scanner.nextInt();
+            scanner.nextLine(); // handle next line character (enter key)
+
+            System.out.print("Enter your name: ");
+            String name = scanner.nextLine();
+
+            if (age > 0 && age <= 100) {
+                System.out.println("Your name = " + name + ", age = " + age);
+            } else {
+                System.out.println("Invalid age");
+            }
+        } else {
+            System.out.println("Unable to parse age");
+        }
 
 
 
-        System.out.println("Your name = " + name);
-        System.out.println("Your age = " + age);
         scanner.close();
     }
 }
