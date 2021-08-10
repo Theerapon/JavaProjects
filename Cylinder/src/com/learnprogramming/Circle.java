059ff57d439b4cf8d0521916a8000809f536eff9
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-08-10 17:22:27 +0700

    Exercise Cylinder

---
+++ Cylinder/src/com/learnprogramming/Circle.java
@@ -0,0 +1,18 @@
+package com.learnprogramming;
+
+public class Circle {
+
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = (radius < 0) ? 0 : radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+}
